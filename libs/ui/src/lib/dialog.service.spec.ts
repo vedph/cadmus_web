54f383f78aa758e63a67d03d0ec41af2e38ad346
phylax <dfusi@hotmail.com>
2019-11-26 19:32:59 +0100

    Attempts to fix dialog specs for injection

--- libs/ui/src/lib/dialog.service.spec.ts
+++ libs/ui/src/lib/dialog.service.spec.ts
@@ -1,11 +1,28 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { DialogService } from './dialog.service';
+import { Provider } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 describe('DialogService', () => {
+  const mockMatDialog = {
+    closeAll: (): void => undefined
+  };
+
   beforeEach(() => {
+    const mockProviders: Provider[] = [
+      { provide: MatDialog, useValue: mockMatDialog },
+      // https://github.com/angular/components/issues/8419
+      {
+        provide: MatDialogRef,
+        useValue: {
+          close: (dialogResult: any) => { }
+        }
+      }
+    ];
+
     TestBed.configureTestingModule({
-      providers: [DialogService]
+      providers: [mockProviders, DialogService]
     });
   });
 
--- libs/ui/src/lib/ui.module.spec.ts
+++ libs/ui/src/lib/ui.module.spec.ts
@@ -1,10 +1,29 @@
 import { async, TestBed } from '@angular/core/testing';
 import { UiModule } from './ui.module';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { Provider } from '@angular/core';
 
 describe('UiModule', () => {
+  // https://stackoverflow.com/questions/56299501/how-to-inject-matdialog-in-a-test-module
+  const mockMatDialog = {
+    closeAll: (): void => undefined
+  };
+
   beforeEach(async(() => {
+    const mockProviders: Provider[] = [
+      { provide: MatDialog, useValue: mockMatDialog },
+      // https://github.com/angular/components/issues/8419
+      {
+        provide: MatDialogRef,
+        useValue: {
+          close: (dialogResult: any) => {}
+        }
+      }
+    ];
+
     TestBed.configureTestingModule({
-      imports: [UiModule]
+      imports: [UiModule],
+      providers: [mockProviders]
     }).compileComponents();
   }));
 
