6b2f227512e18f19b1953fb29146eb73f7426798
phylax <dfusi@hotmail.com>
2019-11-27 18:17:40 +0100

    Completing part editor demo

--- libs/ui/src/lib/components/part-editor-demo/part-editor-demo.component.ts
+++ libs/ui/src/lib/components/part-editor-demo/part-editor-demo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ContentChild } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { PartEditorBaseComponent } from '../part-editor-base/part-editor-base.component';
 
 @Component({
   selector: 'cadmus-part-editor-demo',
@@ -7,24 +8,26 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./part-editor-demo.component.css']
 })
 export class PartEditorDemoComponent implements OnInit {
-  private _partJson: string;
+  private _json: string;
 
   // the ng-content child editor must be decorated with #editor!
   // (this is not a CSS selector:
   // https://stackoverflow.com/questions/51673978/how-do-i-select-a-contentchild-of-a-native-element-in-angular-6)
   @ContentChild('editor', { static: true })
-  private _editor;
+  private _editor: PartEditorBaseComponent;
 
+  /**
+   * The JSON code representing the part being edited.
+   */
   @Input()
-  public get partJson(): string {
-    return this._partJson;
+  public get json(): string {
+    return this._json;
   }
-  public set partJson(value: string) {
-    if (this._partJson === value) {
+  public set json(value: string) {
+    if (this._json === value) {
       return;
     }
-    this._partJson = value;
-    // TODO: setup UI
+    this._json = value;
   }
 
   public editorOptions = {
@@ -35,23 +38,27 @@ export class PartEditorDemoComponent implements OnInit {
   };
 
   public codeForm: FormGroup;
-  public json: FormControl;
+  public jsonEditor: FormControl;
 
   constructor(formBuilder: FormBuilder) {
-    this.json = formBuilder.control(null);
+    this.jsonEditor = formBuilder.control(null);
     this.codeForm = formBuilder.group({
-      json: this.json
+      jsonEditor: this.jsonEditor
     });
   }
 
   ngOnInit() {
     // subscribe to the editor JSON change event
     // https://stackoverflow.com/questions/41858516/capturing-events-emitted-from-components-inside-ng-content
-    // TODO:
+    this._editor.jsonChange.subscribe((json: string) => {
+      this.jsonEditor.setValue(json);
+    });
   }
 
-  public setJson() {
-
-    // TODO: set JSON
+  public setJsonFromCode() {
+    if (this.codeForm.invalid) {
+      return;
+    }
+    this.json = this.jsonEditor.value;
   }
 }
