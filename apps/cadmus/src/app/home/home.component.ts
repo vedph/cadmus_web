7d85893cb8d6223c726d338cbfa73090c8eb9299
naftis <dfusi@hotmail.com>
2019-11-28 16:03:21 +0100

    Minor changes

--- apps/cadmus/src/app/home/home.component.ts
+++ apps/cadmus/src/app/home/home.component.ts
@@ -7,12 +7,22 @@ import { OrthographyFragment } from '@cadmus/parts/philology/philology-ui';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public currentTabIndex: number;
   public partJson: string;
   public thesauriJson: string;
 
   constructor() {
+    this.currentTabIndex = 0;
   }
 
   ngOnInit() {
+  }
+
+  public onCodeSaved() {
+    this.currentTabIndex = 1;
+  }
+
+  public onEditorSaved() {
+    this.currentTabIndex = 0;
   }
 }
