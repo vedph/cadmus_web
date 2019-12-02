e20b25c663cd2167a9aeb848877b924e9846d3b6
naftis <dfusi@hotmail.com>
2019-12-02 13:23:20 +0100

    Added page size to paginator

--- libs/features/feature-item-list/src/lib/item-list/item-list.component.ts
+++ libs/features/feature-item-list/src/lib/item-list/item-list.component.ts
@@ -3,11 +3,12 @@ import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
 import { PaginationResponse, PaginatorPlugin } from '@datorama/akita';
 import { ItemInfo, DataPage, ItemFilter } from '@cadmus/core';
 import { ITEMS_PAGINATOR } from '../services/items-paginator';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap, startWith } from 'rxjs/operators';
 import { ItemsState } from '../state/items.store';
 import { PageEvent } from '@angular/material';
 import { ItemService } from '@cadmus/api';
 import { DialogService } from '@cadmus/ui';
+import { FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'cadmus-item-list',
@@ -17,12 +18,16 @@ import { DialogService } from '@cadmus/ui';
 export class ItemListComponent implements OnInit {
   public pagination$: Observable<PaginationResponse<ItemInfo>>;
   public filter$: BehaviorSubject<ItemFilter>;
+  public pageSize: FormControl;
 
   constructor(
     @Inject(ITEMS_PAGINATOR) public paginator: PaginatorPlugin<ItemsState>,
     private _itemsService: ItemService,
-    private _dialogService: DialogService
-  ) {}
+    private _dialogService: DialogService,
+    formBuilder: FormBuilder
+  ) {
+    this.pageSize = formBuilder.control(20);
+  }
 
   private getRequest(
     filter: ItemFilter
@@ -44,28 +49,42 @@ export class ItemListComponent implements OnInit {
 
   ngOnInit() {
     // filter
+    const initialPageSize = 20;
     this.filter$ = new BehaviorSubject<ItemFilter>(
       this.paginator.metadata.get('filter') || {
         pageNumber: 1,
-        pageSize: 20
+        pageSize: initialPageSize
       }
     );
+    this.pageSize.setValue(initialPageSize);
 
     // combine and get latest:
     // -page number changes from paginator
+    // -page size changes from control
     // -filter changes from filter (in this case, clearing the cache)
     this.pagination$ = combineLatest([
       this.paginator.pageChanges,
+      this.pageSize.valueChanges.pipe(
+        // we are required to emit at least the initial value
+        // as combineLatest emits only if ALL observables have emitted
+        startWith(initialPageSize),
+        // clear the cache when page size changes
+        tap(_ => {
+          this.paginator.clearCache();
+        })
+      ),
       this.filter$.pipe(
         // clear the cache when filters changed
-        tap(_ => this.paginator.clearCache())
+        tap(_ => {
+          this.paginator.clearCache();
+        })
       )
     ]).pipe(
       // for each emitted value, combine into a filter and use it
       // to request the page from server
-      switchMap(([pageNumber, filter]) => {
+      switchMap(([pageNumber, pageSize, filter]) => {
         filter.pageNumber = pageNumber;
-        filter.pageSize = 20;
+        filter.pageSize = pageSize;
         const request = this.getRequest(filter);
         // update saved filters
         this.paginator.metadata.set('filter', filter);
@@ -130,7 +149,9 @@ export class ItemListComponent implements OnInit {
   public pageChanged(event: PageEvent) {
     // https://material.angular.io/components/paginator/api
     this.paginator.setPage(event.pageIndex + 1);
-    // TODO: change size with event.pageSize
+    if (event.pageSize !== this.pageSize.value) {
+      this.pageSize.setValue(event.pageSize);
+    }
   }
 
   public editItem(item: ItemInfo) {
