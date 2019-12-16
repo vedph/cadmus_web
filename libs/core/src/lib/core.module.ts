8b0c55924a3c2a911985be7e68d1a1e34f3b3ab4
naftis <dfusi@hotmail.com>
2019-12-16 13:58:22 +0100

    Upgraded nrwl and angular to 8.2.0

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, InjectionToken } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
--- libs/core/src/lib/services/error.service.spec.ts
+++ libs/core/src/lib/services/error.service.spec.ts
@@ -3,10 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { ErrorService } from './error.service';
 
 describe('ErrorService', () => {
-  beforeEach(() => {
-    TestBed.resetTestEnvironment();
-    TestBed.configureTestingModule({});
-  });
+  beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service = TestBed.get(ErrorService);
