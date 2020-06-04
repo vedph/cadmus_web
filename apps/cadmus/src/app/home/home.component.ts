3b47d55502b547c58df5e0c407093d9ce2ed97bc
Myrmex <dfusi@hotmail.com>
2020-06-04 10:04:27 +0200

    Simpler home

--- apps/cadmus/src/app/home/home.component.ts
+++ apps/cadmus/src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { EnvService } from '@cadmus/core';
+import { AuthService } from '@cadmus/api';
 
 @Component({
   selector: 'cadmus-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   public title: string;
+  public logged: boolean;
 
-  constructor(env: EnvService) {
+  constructor(env: EnvService, authService: AuthService) {
     this.title = env.name;
+    this.logged = authService.currentUserValue !== null;
   }
 }
