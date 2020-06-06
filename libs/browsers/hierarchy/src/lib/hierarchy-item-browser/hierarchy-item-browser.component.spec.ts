ad626f8beb24ebfed8b9637cbfc5fb2785ba0a98
phylax <dfusi@hotmail.com>
2020-06-06 16:37:42 +0200

    Fixed hierarchy item browser tests

--- libs/browsers/hierarchy/src/lib/hierarchy-item-browser/hierarchy-item-browser.component.spec.ts
+++ libs/browsers/hierarchy/src/lib/hierarchy-item-browser/hierarchy-item-browser.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HierarchyItemBrowserComponent } from './hierarchy-item-browser.component';
+import { CoreModule } from '@cadmus/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiModule } from '@cadmus/api';
+import { MaterialModule } from '@cadmus/material';
+import { UiModule } from '@cadmus/ui';
+import { GeneralUiModule } from '@cadmus/parts/general/general-ui';
+import { EditStateModule } from '@cadmus/features/edit-state';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HierarchyItemBrowserComponent', () => {
   let component: HierarchyItemBrowserComponent;
@@ -8,9 +17,21 @@ describe('HierarchyItemBrowserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HierarchyItemBrowserComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterTestingModule,
+        CoreModule,
+        ApiModule,
+        CoreModule,
+        MaterialModule,
+        UiModule,
+        GeneralUiModule,
+        EditStateModule,
+      ],
+      declarations: [HierarchyItemBrowserComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
